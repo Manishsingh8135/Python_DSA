e75f1e0075282f3b5e9708635447b8d21eb78112
Manishsingh8135 <manishsingh8135@gmail.com>
2022-07-31 09:21:35 +0530

    binary search template

---
+++ Binary Search/Template.py
@@ -0,0 +1,17 @@
+
+def BinarySearch(nums,target):
+    left,right=0,len(nums)-1
+
+    while left<right:
+        mid=left+(right-left)//2
+
+        if nums[mid]<target:
+            left=mid+1
+        else:
+            right=mid
+    return left
+
+arr=[2,3,4,5,6,7]
+target=3
+a=BinarySearch(arr,target)
+print(a)
