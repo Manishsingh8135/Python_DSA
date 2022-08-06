90cb18f1216d6a6698d1906d0aecd8dcc2c02af3
Manishsingh8135 <manishsingh8135@gmail.com>
2022-08-06 19:42:54 +0530

    added check

--- Binary Search/Template.py
+++ Binary Search/Template.py
@@ -1,4 +1,7 @@
 
+# This template is for general Binary Search , where you get a target to search
+#Note that this template doesn't tell whether, the target element exists or not.
+    #For this an extra check if required
 def BinarySearch(nums,target):
     left,right=0,len(nums)-1
 
@@ -9,8 +12,11 @@ def BinarySearch(nums,target):
             left=mid+1
         else:
             right=mid
-    print(left)
-    return left
+    #To check whether the target exists in the given array
+    if target==nums[left]:
+        return left
+    else:
+        return -1
 
 N=int(input())
 arr=list(map(int,input().split()))
